409b2b2051fb2fb5b91b5b5ce433913ed0d1069a
noordseoostende <voropayev1@mail.ru>
2021-07-23 19:11:38 +0200

    volgende stap

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>Over</h1>
+      <p>Lorem, ipsum.</p>
+      <p>Lorem, ipsum. 2.00</p>
+    </div>
+  )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddEventsPage() {
+  return (
+    <div>
+      <h1>Add event</h1>
+    </div>
+  )
+}
