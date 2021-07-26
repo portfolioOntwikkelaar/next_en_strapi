75959d90b8b388ea50a7b5f32b179ed1811dc7cd
noordseoostende <voropayev1@mail.ru>
2021-07-26 15:13:52 +0200

    context

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa"
 import { GiHandBandage } from 'react-icons/gi'
 import Search from './Search'
 import Link from 'next/link'
@@ -23,6 +24,12 @@ export default function Header() {
           <li>
             <Link href='/events/add'>
               <a>Toevoegen BPH</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/account/login'>
+              <a className='btn-secondary btn-icon'>
+                <FaSignInAlt />Inloggen</a>
             </Link>
           </li>
         </ul>
---
+++ pages/account/register.js
@@ -0,0 +1,62 @@
+import { FaUser } from "react-icons/fa"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect, useContext } from 'react'
+import Link from "next/link"
+import Layout from '@/components/Layout'
+import styles from '@/styles/AuthForm.module.css'
+
+export default function RegisterPage() {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  
+
+  if(password !== passwordConfirm) {
+    toast.error('Wachtwoord is verkeerd!')
+    return
+  }
+  console.log(username, email, password)
+}
+  return (
+    <Layout title='Gebruiker Aanmelden'>
+      <div className={styles.auth}>
+        <h1>
+          <FaUser /> Aanmelden
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="username">Gebruikersnaam</label>
+            <input type="text" name="username" id='username' 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)}  />
+          </div>
+          <div>
+            <label htmlFor="email">Email Address</label>
+            <input type="email" name="email" id='email' value={email} 
+            onChange={(e) => setEmail(e.target.value)}  />
+          </div>
+          <div>
+            <label htmlFor="password">Wachtwoord</label>
+            <input type="password" name="password" id='password' 
+            onChange={(e) => setPassword(e.target.value)}  />
+          </div>
+          <div>
+            <label htmlFor="passwordConfirm">Bevestigen Wachtwoord</label>
+            <input type="password" name="passwordConfirm" id="passwordConfirm"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}  />
+          </div>
+
+          <input type="submit" value="Login" className='btn'/>
+        </form>
+        <p>Heb je een account?<Link href='/account/login'>Inloggen</Link></p>
+      </div>
+    </Layout>
+  )
+}
