42d0bf987b3168b3111a2e1c5ef76941d2758e5d
noordseoostende <voropayev1@mail.ru>
2021-07-30 19:02:29 +0200

    nog 30%

--- components/Blocks.js
+++ components/Blocks.js
@@ -1,26 +1,54 @@
-import styles from '@/styles/Blocks.module.css'
-import { AiTwotonePhone } from "react-icons/ai"
+import styles from '@/styles/Block2.module.css'
+import { FaSearchLocation } from "react-icons/fa"
 import { MdHome } from "react-icons/md"
 
 export default function Blocks() {
   return (
-    <div>
-      <div className={styles.conta}>
-        <div className={styles.block2}>
-          <AiTwotonePhone size={50} />
-          <h1>CUSTOMER FOCUS</h1>
-          <h3>BY ANTICIPATING MARKET TRENDS AND INVESTING TIME AND EFFORT TO FULLY UNDERSTAND CUSTOMER NEEDS, WE GO TO GREAT LENGTHS TO PROVIDE PRODUCTS AND SOLUTIONS OF SUPERIOR VALUE.</h3>
-        </div>
-        <div className={styles.block2}> <MdHome size={50} />
-          <h1>CUSTOMER FOCUS</h1>
-          <h3>BY ANTICIPATING MARKET TRENDS AND INVESTING TIME AND EFFORT TO FULLY UNDERSTAND CUSTOMER NEEDS, WE GO TO GREAT LENGTHS TO PROVIDE PRODUCTS AND SOLUTIONS OF SUPERIOR VALUE.</h3></div>
-        <div className={styles.block2}> <AiTwotonePhone size={50} />
-          <h1>CUSTOMER FOCUS</h1>
-          <h3>BY ANTICIPATING MARKET TRENDS AND INVESTING TIME AND EFFORT TO FULLY UNDERSTAND CUSTOMER NEEDS, WE GO TO GREAT LENGTHS TO PROVIDE PRODUCTS AND SOLUTIONS OF SUPERIOR VALUE.</h3></div>
-        <div className={styles.block2}> <AiTwotonePhone size={50} />
-          <h1>CUSTOMER FOCUS</h1>
-          <h3>BY ANTICIPATING MARKET TRENDS AND INVESTING TIME AND EFFORT TO FULLY UNDERSTAND CUSTOMER NEEDS, WE GO TO GREAT LENGTHS TO PROVIDE PRODUCTS AND SOLUTIONS OF SUPERIOR VALUE.</h3></div>
-      </div>
+    <div className={styles.backgrnd}>
+      
+      <div className={styles.card}>
+        
+      
+      <p><br /></p>
+      <FaSearchLocation className={styles.iconi} size={80} />
+  <h1>CUSTOMER FOCUS</h1>
+  <p className={styles.price}>BY ANTICIPATING MARKET TRENDS AND INVESTING TIME AND EFFORT TO FULLY UNDERSTAND CUSTOMER NEEDS, WE GO TO GREAT LENGTHS TO PROVIDE PRODUCTS AND SOLUTIONS OF SUPERIOR VALUE.</p>
+  <p><br /></p>
+  
+</div>
+<div className={styles.card}>
+<p><br /></p>
+<FaSearchLocation className={styles.iconi} size={80} />
+  <h1>CUSTOMER FOCUS</h1>
+  <p className={styles.price}>BY ANTICIPATING MARKET TRENDS AND INVESTING TIME AND EFFORT TO FULLY UNDERSTAND CUSTOMER NEEDS, WE GO TO GREAT LENGTHS TO PROVIDE PRODUCTS AND SOLUTIONS OF SUPERIOR VALUE.</p>
+  <p><br /></p>
+  
+</div>
+<div className={styles.card}>
+<p><br /></p>
+<FaSearchLocation className={styles.iconi} size={80} />
+  <h1>CUSTOMER FOCUS</h1>
+  <p className={styles.price}>BY ANTICIPATING MARKET TRENDS AND INVESTING TIME AND EFFORT TO FULLY UNDERSTAND CUSTOMER NEEDS, WE GO TO GREAT LENGTHS TO PROVIDE PRODUCTS AND SOLUTIONS OF SUPERIOR VALUE.</p>
+  <p><br /></p>
+  
+</div>
     </div>
   )
 }
+{/* <div className={styles.conta}>
+      <h1 className={styles.waarden}>ONZE WAARDEN</h1>
+        <div className={styles.block3}>
+          <FaSearchLocation className={styles.iconi} size={80} />
+          <h1 className={styles.texxt}>CUSTOMER FOCUS</h1>
+          <h3 className={styles.texxt}>BY ANTICIPATING MARKET TRENDS AND INVESTING TIME AND EFFORT TO FULLY UNDERSTAND CUSTOMER NEEDS, WE GO TO GREAT LENGTHS TO PROVIDE PRODUCTS AND SOLUTIONS OF SUPERIOR VALUE.</h3>
+        </div>
+        <div className={styles.block2}> <MdHome className={styles.iconi} size={80} />
+          <h1 className={styles.texxt}>CUSTOMER FOCUS</h1>
+          <h3 className={styles.texxt}>MET HET DOEL OM EEN SUPERIEURE KLANTENWAARDE TE CREREN, DAGEN WE DE STATUS QUO CONSTRUCTIEF UIT EN ONDERZOEKEN KANSEN OM TE INNOVEREN. WIJ ZIJN BEREID OM GECONTROLEERDE RISICO'S TE NEMEN EN ZIJN VASTBESLOTEN OM TE VOLDOEN.</h3></div>
+        <div className={styles.block2}> <MdHome className={styles.iconi} size={80} />
+          <h1 className={styles.texxt}>CUSTOMER FOCUS</h1>
+          <h3 className={styles.texxt}>ONZE TROTS OP WAT ONS BEDRIJF ONS INSPIRERT EN MOTIVEERT OM ONS BEST OP HET WERK TE GEVEN. WIJ STAAN GRAAG OM OVER ONS BEDRIJF TE LEREN EN OM ONZE KENNIS EN ENTHOUSIASME MET ANDEREN TE DELEN.</h3></div>
+        <div className={styles.block4}> <MdHome className={styles.iconi} size={80} />
+          <h1 className={styles.texxt}>CUSTOMER FOCUS</h1>
+          <h3 className={styles.texxt}>OF OP HET VELD BIJ KLANTEN, OP KANTOOR, OP DE WEG - WIJ ZIJN ÉÉN TEAM, DELEN EEN GEMEENSCHAPPELIJK DOEL EN GEMEENSCHAPPELIJKE DOELEN. OPEN COMMUNICATIE, IDEE DELEN, GEMEENSCHAPPELIJKE DOELEN BEREIKEN ZIJN DE SLEUTEL!</h3></div>
+  </div> */}
--- components/Footer.js
+++ components/Footer.js
@@ -3,18 +3,47 @@ import styles from '@/styles/Footer.module.css'
 import { AiTwotonePhone } from "react-icons/ai"
 import { MdHome } from "react-icons/md"
 import Image from 'next/image'
-import profilePic from '../public/tnd-group.png'
+
+import profilePic from '../public/bvbo_1.jpg'
+import profilePic1 from '../public/trends.jpg'
+import profilePic2 from '../public/group.png'
 
 
 
 export default function Footer() {
   
   return (
-    <footer className={styles.footer}>
+    <footer >
+      <div className={styles.lijn}>
+       
+      </div>
+      <div className={styles.footer} >
       
       <div className={styles.paragraf}>
-      <p>T&D security bvba met als identificatienummer 'BE 0830 397 501' bij de Kruispuntbank van Ondernemingen is verzekerd tegen lichamelijke of materiële schade die voortvloeien uit  de uitoefening van zijn activiteiten inzake private veiligheid. De benadeelden kunnen zich rechtstreeks wenden tot AG insurance te Emile Jacqmainlaan 53, 1000 Brussel. </p>
-      </div>
+        <div className={styles.paragraf5}>
+        <Image className={styles.paragraf4}
+        src={profilePic}
+        alt="Picture of the author"
+        width={'300 px'}
+        height={'80 px'}
+      />
+        <Image className={styles.paragraf4}
+        src={profilePic1}
+        alt="Picture of the author"
+        width={'300 px'}
+        height={'80 px'}
+      /></div>
+      <div className={styles.paragraf9}>
+        <Image 
+        src={profilePic2}
+        alt="Picture of the author"
+        width={'300 px'}
+        height={'80 px'}
+      /></div>
+        
+        <div className={styles.paragraf2}>
+      <p>T&D security bvba met als identificatienummer 'BE 0830 397 501' bij de Kruispuntbank van Ondernemingen<br /> is verzekerd tegen lichamelijke of materiële schade  die voortvloeien uit  de uitoefening van zijn activiteiten inzake private veiligheid.<br /> De benadeelden kunnen zich rechtstreeks wenden tot AG insurance te Emile Jacqmainlaan 53, 1000 Brussel. </p></div>
+      </div></div>
     </footer>
   )
 }
